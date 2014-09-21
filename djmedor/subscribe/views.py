30b438cb85b26702dbb5bb86e7c733eb85cf03a1
Alexandre Leray <alexandre@stdin.fr>
2014-09-22 01:27:13 +0200

    Step 2 du formulaire cooperation

--- djmedor/subscribe/views.py
+++ djmedor/subscribe/views.py
@@ -5,12 +5,34 @@ from django.shortcuts import render_to_response
 from django.contrib.formtools.wizard.views import CookieWizardView
 
 
+COOPERATION_FORMS = [
+    CooperationForm,
+    ConfirmForm,
+]
+
+COOPERATION_TEMPLATES = [
+    "subscribe/cooperation-registration.html",
+    "subscribe/cooperation-confirmation.html",
+]
+
 
 class CooperationWizardView(CookieWizardView):
-    template_name = "subscribe/wizard.html"
-    form_list = [CooperationForm, ConfirmForm]
+    form_list = COOPERATION_FORMS
 
-    def done(self, form_list, **kwargs):
+    def get_context_data(self, form, **kwargs):
+        context = super(CooperationWizardView, self).get_context_data(form=form, **kwargs)
+        print(self.steps.current)
+        if self.steps.current == '1':
+            context.update({
+                'infos': self.get_cleaned_data_for_step("0"),
+                'total': self.get_cleaned_data_for_step("0")['share_number'] * 20
+            })
+        return context
+
+    def get_template_names(self):
+        return [COOPERATION_TEMPLATES[int(self.steps.current)]]
+
+    def done(self, form_list, form_dict, **kwargs):
         return render_to_response('done.html', {
             'form_data': [form.cleaned_data for form in form_list],
         })
