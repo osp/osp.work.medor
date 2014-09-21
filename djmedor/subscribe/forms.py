b04b40a27d0cf8259a2ed7c8080a2e1d206b9982
Alexandre Leray <alexandre@stdin.fr>
2014-09-21 18:28:36 +0200

    proto wizard

--- djmedor/subscribe/forms.py
+++ djmedor/subscribe/forms.py
@@ -1,10 +1,17 @@
-from django.forms import ModelForm
+from django import forms
 from subscribe.models import Cooperator
 
 
-class CooperateForm(ModelForm):
+class CooperateForm(forms.ModelForm):
     error_css_class = 'error'
     required_css_class = 'required'
 
     class Meta:
         model = Cooperator
+
+
+class ConfirmForm(forms.Form):
+    error_css_class = 'error'
+    required_css_class = 'required'
+
+    confirm = forms.BooleanField()
