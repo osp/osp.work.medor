588fa0734e5baf4b0abcb7261a377930fc240605
Stéphanie Vilayphiou <stephanie@stdin.fr>
2014-09-21 18:39:49 +0200

    traduction des champs coopérateur

--- djmedor/subscribe/models.py
+++ djmedor/subscribe/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
@@ -25,23 +26,23 @@ class Cooperator(models.Model):
         (6, u'6 (120)')
     )
 
-    first_name = models.CharField(_('first name'), max_length=30)
-    last_name = models.CharField(_('last name'), max_length=30)
-    email = models.EmailField(_('email address'))
-    birth_date = models.DateTimeField(_('birth date'))
-    title = models.BooleanField(_('title'), default=False, choices=TITLE_CHOICES)
+    first_name = models.CharField('prénom', max_length=30)
+    last_name = models.CharField('nom de famille', max_length=30)
+    email = models.EmailField('courriel')
+    birth_date = models.DateTimeField('date de naissance')
+    title = models.BooleanField('civilité', default=False, choices=TITLE_CHOICES)
 
-    nationality = models.CharField(_('nationality'), max_length=2, choices=COUNTRY_CHOICES)
-    id_number = models.CharField(_('ID number'), max_length=30)
+    nationality = models.CharField('nationalité', max_length=2, choices=COUNTRY_CHOICES)
+    id_number = models.CharField("numéro d'identité nationale", max_length=30)
 
-    street = models.CharField(_('street'), max_length=30)
-    number = models.CharField(_('number'), max_length=10) # 27 bis
-    letterbox = models.PositiveSmallIntegerField(_('letterbox'), max_length=30, null=True, blank=True)
-    city = models.CharField(_('city'), max_length=30)
-    zip_code = models.PositiveSmallIntegerField(_('zip code'), max_length=5)
-    country = models.CharField(_('nationality'), max_length=2, choices=COUNTRY_CHOICES)
-    phone_number = PhoneNumberField(blank=True)
-    share_number = models.PositiveSmallIntegerField(_('share number'), choices=SHARE_CHOICES)
+    street = models.CharField('rue', max_length=30)
+    number = models.CharField('numéro', max_length=10) # 27 bis
+    letterbox = models.PositiveSmallIntegerField('boîte postale', max_length=30, null=True, blank=True)
+    city = models.CharField('ville', max_length=30)
+    zip_code = models.PositiveSmallIntegerField('code postal', max_length=5)
+    country = models.CharField('pays', max_length=2, choices=COUNTRY_CHOICES)
+    phone_number = PhoneNumberField(blank=True, verbose_name='numéro de téléphone')
+    share_number = models.PositiveSmallIntegerField('nombre de parts', choices=SHARE_CHOICES)
 
 
     class Meta:
